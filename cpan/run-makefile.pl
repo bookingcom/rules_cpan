6ee83cb56ec48ec64a9deec4a8c5bd30edb1d5ad
Manuel Naranjo <naranjo.manuel@gmail.com>
2025-02-22 20:20:28 +0100

    bazel: generate Makefile in a reproducible way
    Make sure we generate Makefile with Makefile.PL and touch the file after
    generation to make sure it's reproducible

--- cpan/run-makefile.pl
+++ cpan/run-makefile.pl
@@ -6,30 +6,47 @@ use Cwd qw/cwd abs_path/;
 use File::Basename qw/dirname basename/;
 use File::Copy qw/copy/;
 use File::Path qw(make_path);
+use Config qw/config_vars/;
+use Config;
+
+BEGIN {
+    push @INC, dirname(__FILE__);
+}
+
+use PerlToolchain qw/PERL_TOOLCHAIN_PATH/;
+
+sub read_file($filename) {
+    open(my $f, '<', $filename) or die "OPENING $filename: $!\n";
+    local $/;
+    return <$f>;
+}
+
+sub write_file($filename, $string) {
+    open(my $f, '>', $filename) or die "OPENING $filename: $!\n";
+    print $f $string;
+    close($f);
+}
 
 my $CWD = cwd;
-use Data::Dumper;
 
 chdir(dirname($ENV{PATH_TO_SCRIPT}));
 
-say STDERR Dumper(\%ENV);
-say STDERR abs_path('.');
-say STDERR $^X;
-die 1;
+system("$^X Makefile.PL PREFIX=$ENV{BINDIR}/lib/perl5") == 0 or die "ERROR: Makefile.PL failed: $!";
 
-system("$^X Makefile.PL SITELIBEXP=foo > /dev/stderr") == 0 or die "ERROR: Makefile.PL failed: $!";
+( my $perl_toolchain = PERL_TOOLCHAIN_PATH ) =~ s!^external/!!g;
+$perl_toolchain =~ s/\+/\\+/g;
 
-#my @lines = path('Makefile')->lines_utf8;
+( my $external_path = $INC{'Config.pm'} ) =~ s{(.*/external)(/?.*)}{$1}g;
 
-#@lines = map { my @parts = split() } @lines;
+my $makefile = read_file('Makefile');
+$makefile =~ s!(${external_path}/${perl_toolchain}/)!$ENV{BINDIR}/lib/perl5/!gm;
 
-
+write_file('Makefile', $makefile);
 
 for my $path (@ARGV) {
     my $file = basename($path);
     my $dirname = dirname($path);
     make_path("${CWD}/${dirname}") unless -e "${CWD}/${dirname}";
-
     copy($file, "${CWD}/${path}") or die "ERRROR: failed to copy ${path}: $!";
 }
 
